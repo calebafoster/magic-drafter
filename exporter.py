17e642e8b525b83c860427225f4d4c6830d83bf5
Caleb Foster <calebalakaifoster@gmail.com>
2025-01-07 16:02:03 -0800

    changed the name of the exported deck

--- exporter.py
+++ exporter.py
@@ -6,14 +6,15 @@ map = {"W": "Plains", "U": "Island", "B": "Swamp", "R": "Mountain", "G": "Forest
 class Exporter:
     def __init__(self, deck):
         self.deck = deck
-        self.color_identity = self.deck[0].dict['color_identity']
+        self.commander = self.deck[0]
+        self.color_identity = self.commander.dict['color_identity']
         self.string = ""
 
     def export(self):
         for card in self.deck:
             self.string += f"1 {card.dict['name']}\n"
 
-        with open("deck.txt", 'w') as f:
+        with open(f"{self.commander.dict['name']}.txt", 'w') as f:
             f.write(self.string)
 
         pygame.quit()
